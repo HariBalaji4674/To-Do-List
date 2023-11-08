6517a6c2da624bd42c82099881403580d5979a72
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-08 11:39:09 +0530

    Add new files

---
+++ 08-November-2023.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+06:00 am -- DevOps Interview Questions (min - 10 Q-s) --> Done
+06:30 am -- Kubernetes Abhishek Video
+07:00 am -- Kubernetes Session 57
+09:00 am -- Bursh and Bath
+09:30 am -- Again Kubernetes
+10:30 am -- CICD Abhishek Video 
+11:00 am -- Session 42
+01:00 pm -- Lunch Break
+02:00 pm -- Terraform Session 32
+04:00 pm -- Terraform Session 33
+06:00 pm -- Computer Networks ,ARP,BroaderGateway 
+07:00 pm -- Shell Script 13
+10:00 pm -- Sleep.
