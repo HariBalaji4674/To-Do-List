f25ad3394ef70383f780c05bc5b91cfe1ab408cb
HariBalaji4674 <Hari.Balaji4674@gmail.com>
2023-11-12 08:27:03 +0530

    adding new files

--- 12-November-2023.sh
+++ 12-November-2023.sh
@@ -1,12 +1,5 @@
+#!/bin/bash
 
-05 am --> Docker Session - 53 
-07 am --> Docker Session - 54  
-09 am --> Brush Bath Meditation BreakFast
-10 am --> Docker Session - 55 
-12 pm --> Docker Session - 56
-02 pm --> Lunch Break and Nap
-03 pm --> Kubernetes Session - 57
-05 pm --> Kubernetes Session - 58
-07 pm --> Kubernetes Session - 59
-08 pm --> Kubernetes Session - 60
-# git add .;git commit -m "Adding New Files" ;git push origin main
+10 am -- Git  Interview Questions
+02 pm -- linux Interview Questions
+08 pm -- Shell Scripting GitHub + GeekForGeeks
---
+++ 13-November-2023.sh
@@ -0,0 +1,13 @@
+04 am --> Docker Session - 52
+05 am --> Docker Session - 53 
+07 am --> Docker Session - 54  
+09 am --> Brush Bath Meditation BreakFast
+10 am --> Docker Session - 55 
+12 pm --> Docker Session - 56
+02 pm --> Lunch Break and Nap
+03 pm --> Kubernetes Session - 57
+05 pm --> Kubernetes Session - 58
+08 pm --> Kubernetes Session - 59
+09 pm --> Kubernetes Session - 60
+
+# git add .;git commit -m "Adding New Files" ;git push origin main
